6617ef22c550f3e3d17584fd9504ad6d46db72e1
Lukasz Marwitz <lukaszmarwitz@gmail.com>
2019-02-08 16:48:58 +0100

    POST request UTF-8 encoding fix and loggers cleanup

--- src/main/java/com/wooky/core/StaticFields.java
+++ src/main/java/com/wooky/core/StaticFields.java
@@ -10,7 +10,7 @@ public class StaticFields {
 
     private static StaticFields staticFields;
     private static final Logger LOG = LoggerFactory.getLogger(StaticFields.class);
-    private static final String PATTERN_NAME = "[a-zA-ZąćęłńóśżźĄĆĘŁŃÓŚŻŹäöüäöüÄÖÜß]+";
+    private static final String PATTERN_NAME = "[a-zA-ZąćęłńóśżźĄĆĘŁŃÓŚŻŹäöüÄÖÜß]+";
     private static final String PATTERN_EMAIL = "^[a-zA-Z0-9][a-zA-Z0-9._%+-]{0,63}@(?:[a-zA-Z0-9-]{1,63}\\.){1,8}[a-zA-Z]{2,4}$";
     private static final String PATTERN_PHONE = "[\\d]+";
     private static final String PATTERN_BIRTHDATE = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$";
