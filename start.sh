2b8eba58b8f004fb8ae103b22112da3689c0bd3f
Lukasz Marwitz <lukaszmarwitz@gmail.com>
2019-07-26 17:06:57 +0200

    improved start script

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
-mvn clean package
-docker-compose up -d --build --force-recreate
+#!/usr/bin/env bash
+
+mvn_command="mvn clean package"
+
+${mvn_command}
+
+status="$?"
+
+if [ ${status} -ne 0 ]; then
+    echo "Script could not perform [$mvn_command] and returned exit code [$status]"
+    exit ${status}
+else
+    docker-compose up -d --build --force-recreate
+fi
